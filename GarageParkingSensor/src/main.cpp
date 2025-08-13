fa8b8dfee8f7a725a70ee21af205d68871009e4f
Manav Karonde <karonde.manav@gmail.com>
2025-08-13 12:20:30 -0500

    some code to test two tof sensors on the same i2c port

--- GarageParkingSensor/src/main.cpp
+++ GarageParkingSensor/src/main.cpp
@@ -1,171 +1,179 @@
-#include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
-#include <Adafruit_VL53L0X.h>
+// #include <Arduino.h>
+// #include <Adafruit_NeoPixel.h>
+// #include <Adafruit_VL53L0X.h>
 
-// Pin where the NeoPixel data line is connected
-#define LED_PIN    18      // Use GPIO 5
+// // Pin where the NeoPixel data line is connected
+// #define LED_PIN    18      // Use GPIO 5
 
-// Number of LEDs in the grid (e.g. 8x8 = 64)
-#define NUM_LEDS   32
+// // Number of LEDs in the grid (e.g. 8x8 = 64)
+// #define NUM_LEDS   32
 
-// Create NeoPixel object
-Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+// /*
+// Trig = 35
+// Echo = 34
 
-// Create Distance Sensor object
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+// Left XSHUT = 14
+// Right XSHUT = 13
+// */
 
-/*
-TODO:
--include lighting animations when on approach
--make distance lights detect backing out as well
--error handling to determine if bad data is being returned from sensor
-*/
+// // Create NeoPixel object
+// Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Sensor ranges
-int GREEN_LIMIT = 750;
-int YELLOW_LIMIT = 350;
-int STOOOPPPP = 150;
+// // Create Distance Sensor object
+// Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-void setup() {
-  strip.begin();           // Initialize the NeoPixel strip
-  strip.show();            // Turn off all LEDs initially
-  strip.setBrightness(50); // Optional: set brightness (0–255)
-  Serial.begin(115200);
+// /*
+// TODO:
+// -include lighting animations when on approach
+// -make distance lights detect backing out as well
+// -error handling to determine if bad data is being returned from sensor
+// */
 
-  // wait for the VL53L0X to be ready
-  if (!lox.begin()) {
-    Serial.println("Failed to find VL53L0X chip");
-    while (1);
-  }
-}
+// // Sensor ranges
+// int GREEN_LIMIT = 750;
+// int YELLOW_LIMIT = 350;
+// int STOOOPPPP = 150;
 
-// take a reading from the sensor
-int distance_reading()
-{
-  // read distance from sensor
-  VL53L0X_RangingMeasurementData_t measure;
-  lox.rangingTest(&measure, false);
+// void setup() {
+//   strip.begin();           // Initialize the NeoPixel strip
+//   strip.show();            // Turn off all LEDs initially
+//   strip.setBrightness(50); // Optional: set brightness (0–255)
+//   Serial.begin(115200);
 
-  int distance = measure.RangeMilliMeter;
-  if (measure.RangeStatus == 4) { // make sure reading in range
-    distance = -1;
-  }
+//   // wait for the VL53L0X to be ready
+//   if (!lox.begin()) {
+//     Serial.println("Failed to find VL53L0X chip");
+//     while (1);
+//   }
+// }
 
-  return distance;
-}
+// // take a reading from the sensor
+// int distance_reading()
+// {
+//   // read distance from sensor
+//   VL53L0X_RangingMeasurementData_t measure;
+//   lox.rangingTest(&measure, false);
 
-// when we believe the car is moving
-// returns false if no longer believes car is moving (out of range or passed STOOOOPPPP)
-bool moving_car()
-{
-  int distance = distance_reading();
+//   int distance = measure.RangeMilliMeter;
+//   if (measure.RangeStatus == 4) { // make sure reading in range
+//     distance = -1;
+//   }
 
-  // safe distance
-  if(distance >= GREEN_LIMIT)
-  {
-    for (int i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, strip.Color(0, 255, 0)); // Green
-      strip.show();
-    }
-  }
+//   return distance;
+// }
 
-  // slow down distance
-  if(distance < GREEN_LIMIT && distance >= YELLOW_LIMIT)
-  {
-     for (int i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, strip.Color(220, 180, 24)); // Yellow
-      strip.show();
-    }
-  }
+// // when we believe the car is moving
+// // returns false if no longer believes car is moving (out of range or passed STOOOOPPPP)
+// bool moving_car()
+// {
+//   int distance = distance_reading();
 
-  // get ready to stop distance
-  if(distance < YELLOW_LIMIT && distance >= STOOOPPPP)
-  {
-     for (int i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red
-      strip.show();
-    }
-  }
+//   // safe distance
+//   if(distance >= GREEN_LIMIT)
+//   {
+//     for (int i = 0; i < strip.numPixels(); i++) {
+//       strip.setPixelColor(i, strip.Color(0, 255, 0)); // Green
+//       strip.show();
+//     }
+//   }
 
-  // STOOOOPPP!!!!!
-  if(distance <= STOOOPPPP && distance != -1)
-  {
-    for(int x = 0; x < 10; x++)
-    {
-      for (int i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(255, 0, 0)); // Flashing red
-        strip.show();
-      }
-      delay(50);
-      for (int i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(0, 0, 0)); // off
-        strip.show();
-      }
-      delay(50);
-    }
-    // assume the car has stopped
-    return false;
-  }
+//   // slow down distance
+//   if(distance < GREEN_LIMIT && distance >= YELLOW_LIMIT)
+//   {
+//      for (int i = 0; i < strip.numPixels(); i++) {
+//       strip.setPixelColor(i, strip.Color(220, 180, 24)); // Yellow
+//       strip.show();
+//     }
+//   }
 
-  // out of range, assume there is no car
-  if(distance == -1)
-  {
-    return false;
-  }
+//   // get ready to stop distance
+//   if(distance < YELLOW_LIMIT && distance >= STOOOPPPP)
+//   {
+//      for (int i = 0; i < strip.numPixels(); i++) {
+//       strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red
+//       strip.show();
+//     }
+//   }
 
-  // assume car is still moving
-  return true;
-}
+//   // STOOOOPPP!!!!!
+//   if(distance <= STOOOPPPP && distance != -1)
+//   {
+//     for(int x = 0; x < 10; x++)
+//     {
+//       for (int i = 0; i < strip.numPixels(); i++) {
+//         strip.setPixelColor(i, strip.Color(255, 0, 0)); // Flashing red
+//         strip.show();
+//       }
+//       delay(50);
+//       for (int i = 0; i < strip.numPixels(); i++) {
+//         strip.setPixelColor(i, strip.Color(0, 0, 0)); // off
+//         strip.show();
+//       }
+//       delay(50);
+//     }
+//     // assume the car has stopped
+//     return false;
+//   }
 
-// check if two readings are far apart enough to trigger moving car
-bool readings_differ(int readingA, int readingB)
-{
-  // special cases to check for very far vs out of range readings
-  if(readingA > 1500 && readingB > 1500)
-  {
-    return false;
-  }
-  if(readingA > 1500 && readingB == -1)
-  {
-    return false;
-  }
-  if(readingA == -1 && readingB > 1500)
-  {
-    return false;
-  }
+//   // out of range, assume there is no car
+//   if(distance == -1)
+//   {
+//     return false;
+//   }
 
-  // check if readings are within range
-  int error = readingB - readingA;
-  if(error < 0)
-  {
-    error = -error;
-  }
-  return error > 50;
-}
+//   // assume car is still moving
+//   return true;
+// }
 
-void loop() {
-  // check if movement is occuring
-  int readingA = distance_reading();
-  delay(50);
-  int readingB = distance_reading();
-  if(readings_differ(readingA, readingB))
-  {
-    // assume car is still moving for time to complete loop or until otherwise signaled
-    for(int i = 0; i < 150; i++)
-    {
-      if(!moving_car())
-      {
-        break;
-      }
-    }
-  }
+// // check if two readings are far apart enough to trigger moving car
+// bool readings_differ(int readingA, int readingB)
+// {
+//   // special cases to check for very far vs out of range readings
+//   if(readingA > 1500 && readingB > 1500)
+//   {
+//     return false;
+//   }
+//   if(readingA > 1500 && readingB == -1)
+//   {
+//     return false;
+//   }
+//   if(readingA == -1 && readingB > 1500)
+//   {
+//     return false;
+//   }
 
-  // blue color = standby mode
-  else
-  {
-    for (int i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, strip.Color(0, 0, 255)); // blue
-      strip.show();
-    }
-  }
-}
+//   // check if readings are within range
+//   int error = readingB - readingA;
+//   if(error < 0)
+//   {
+//     error = -error;
+//   }
+//   return error > 50;
+// }
+
+// void loop() {
+//   // check if movement is occuring
+//   int readingA = distance_reading();
+//   delay(50);
+//   int readingB = distance_reading();
+//   if(readings_differ(readingA, readingB))
+//   {
+//     // assume car is still moving for time to complete loop or until otherwise signaled
+//     for(int i = 0; i < 150; i++)
+//     {
+//       if(!moving_car())
+//       {
+//         break;
+//       }
+//     }
+//   }
+
+//   // blue color = standby mode
+//   else
+//   {
+//     for (int i = 0; i < strip.numPixels(); i++) {
+//       strip.setPixelColor(i, strip.Color(0, 0, 255)); // blue
+//       strip.show();
+//     }
+//   }
+// }
---
+++ GarageParkingSensor/src/sensorTest.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_VL53L0X.h>
+#include <Wire.h>
+
+#define XSHUT_LEFT 14
+#define XSHUT_RIGHT 13
+
+
+// Create Distance Sensor object
+Adafruit_VL53L0X leftSensor = Adafruit_VL53L0X();
+Adafruit_VL53L0X rightSensor = Adafruit_VL53L0X();
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(21, 22); // SDA = 21, SCL = 22
+
+    pinMode(XSHUT_LEFT, OUTPUT);
+    pinMode(XSHUT_RIGHT, OUTPUT);
+
+    // turn off both sensors
+    digitalWrite(XSHUT_LEFT, LOW);
+    digitalWrite(XSHUT_RIGHT, LOW);
+    delay(10);
+
+    // power on sensor 1 and set new address
+    digitalWrite(XSHUT_LEFT, HIGH);
+    delay(10);
+    if (!leftSensor.begin(0x30, &Wire)) {
+        Serial.println("Failed to initialize sensor 1");
+        while (1);
+    }
+
+    // power on sensor 2 and set new address
+    digitalWrite(XSHUT_RIGHT, HIGH);
+    delay(10);
+    if (!rightSensor.begin(0x31, &Wire)) {
+        Serial.println("Failed to initialize sensor 2");
+        while (1);
+    }
+}
+
+void loop() {
+    VL53L0X_RangingMeasurementData_t measure1;
+    VL53L0X_RangingMeasurementData_t measure2;
+
+    leftSensor.rangingTest(&measure1, false);
+    rightSensor.rangingTest(&measure2, false);
+
+    Serial.print("Sensor 1: ");
+    if (measure1.RangeStatus != 4)
+        Serial.print(measure1.RangeMilliMeter);
+    else
+        Serial.print("Out of range");
+    
+    Serial.print(" mm\tSensor 2: ");
+    if (measure2.RangeStatus != 4)
+        Serial.print(measure2.RangeMilliMeter);
+    else
+        Serial.print("Out of range");
+    
+    Serial.println(" mm");
+
+    delay(500);
+}
